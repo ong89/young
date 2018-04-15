f94cb04d01b0593bc3306dd87b610051a1c8804e
ong <skdud5381@naver.com>
2018-04-15 15:00:57 +0900

    init

---
+++ young/src/main/java/com/starbucks/young/App.java
@@ -0,0 +1,13 @@
+package com.starbucks.young;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
